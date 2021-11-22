f84cd8930ec26d6bd1285d6d5787a0bffe353999
davidharnisch <davidharnisch@seznam.cz>
2021-11-22 22:10:47 +0100

    Final

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-//Choose a random color
-
-
-
-// document.getElementById('test').onclick = random_bg_color; 
-
-
-// function random_bg_color() {
-//     var x = Math.floor(Math.random() * 256);
-//     var y = Math.floor(Math.random() * 256);
-//     var z = Math.floor(Math.random() * 256);
-//    // var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-
-//    var bgColor = "#" + x + y + z;
-//  console.log(bgColor);
-  
-//  document.body.style.backgroundColor = bgColor; 
-//     }
 
 function makeid(length) {
     var result           = '';
@@ -45,16 +27,3 @@ document.getElementById("addtext").innerHTML = tostring;
 
 
 document.getElementById('test').onclick = random_bg_color; 
-
-// Add hex code to heading
-
-
-// var textToAdd = document.createTextNode(fString);
-// parElement.appendChild(textToAdd);
-
-
-
-
-//function changeColor() {
-//   document.getElementById("container").style.backgroundColor = "rgb(Math.floor(Math.random()),34,56)";
-//}  
