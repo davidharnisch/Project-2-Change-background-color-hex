24ddcc4d428b5df1aa4560604b8753c0efeb694a
davidharnisch <davidharnisch@seznam.cz>
2021-11-22 20:00:26 +0100

    Hotovo stylov8n

--- js/script.js
+++ js/script.js
@@ -2,18 +2,47 @@
 
 
 
+// document.getElementById('test').onclick = random_bg_color; 
+
+
+// function random_bg_color() {
+//     var x = Math.floor(Math.random() * 256);
+//     var y = Math.floor(Math.random() * 256);
+//     var z = Math.floor(Math.random() * 256);
+//    // var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+//    var bgColor = "#" + x + y + z;
+//  console.log(bgColor);
+  
+//  document.body.style.backgroundColor = bgColor; 
+//     }
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'abcdef123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+console.log(makeid(6));
+
+function random_bg_color() {
+var bgColor = "#" + (makeid(6));
+document.body.style.backgroundColor = bgColor; 
+
+console.log(bgColor);
+
+}
+
+
+
 document.getElementById('test').onclick = random_bg_color; 
 
 
-function random_bg_color() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
- console.log(bgColor);
-  
- document.body.style.backgroundColor = bgColor; 
-    }
 
 
 //function changeColor() {
